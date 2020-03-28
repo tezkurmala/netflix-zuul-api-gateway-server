df3021074846ffb7c89823cfa5705ddb76308b90
Tez Kurmala <Tez.Kurmala@jda.com>
2020-03-28 22:13:36 +0530

    enabling api gateway server with logging filter

---
+++ src/main/java/com/boot/services/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,26 @@
+package com.boot.services.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//Tez notes:
+//Normal we access the micro service directly like
+//http://localhost:spring-application-server-port/{request-uri}
+//http://localhost:8100/currency-conversion-feign/from/USD/to/INR/2000
+//The port indicates the spring application name.
+//But when going through zuul gateway, all spring applications go through it.
+//Hence the spring-application-name has to be part of the request URL
+//http://localhost:zuul-port/{spring-application-name}/{request-uri}
+//http://localhost:8765/currency-conversion-service/currency-conversion-feign/from/USD/to/INR/2000
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
